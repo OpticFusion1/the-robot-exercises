48be0ea3b2cf9c9920ab40e65a9f6d6c2993de66
Jlampkins <Jlampkins42@gmail.com>
2019-03-26 23:38:17 -0500

    implemented OOP

--- src/HeroRPG/Enemy.java
+++ src/HeroRPG/Enemy.java
@@ -2,9 +2,31 @@ package HeroRPG;
 
 public class Enemy {
     public String enemyName;
-    public int enemyHealth = 20;
-    public int enemyAttack = 5;
+    private int enemyHealth;
+    private int enemyAttack;
 //    public int enemyPotion = 2;
 //    public int enemypotionCount = 3;
 
+
+    public Enemy(int health, int attack){
+        this.enemyHealth = health;
+        this.enemyAttack = attack;
+    }
+
+
+    public int getEnemyHealth() {
+        return enemyHealth;
+    }
+
+    public int getEnemyAttack() {
+        return enemyAttack;
+    }
+
+    public void setEnemyHealth(int enemyHealth) {
+        this.enemyHealth = enemyHealth;
+    }
+
+    public void setEnemyAttack(int enemyAttack) {
+        this.enemyAttack = enemyAttack;
+    }
 }
--- src/HeroRPG/Hero.java
+++ src/HeroRPG/Hero.java
@@ -1,14 +1,90 @@
 package HeroRPG;
-
+import java.util.Scanner;
 public class Hero {
-    public String heroName;
-    public int heroHealth = 20;
-    public int heroAttack = 2;
-    public int heroPotion = 2;
-    public int potionCount = 3;
-    public int goldPoint = 100;
-    public boolean warrior = false;
-    public boolean mage = false;
+    private String heroName;
+    private int heroHealth;
+    private int heroAttack;
+    private int heroPotion;
+    private int potionCount;
+    private int goldPoint;
+    private boolean warrior;
+    private boolean mage;
 
+
+
+    public Hero(String name, int health, int attack, int potionStrength, int numPotions, int goldPoint, boolean warrior, boolean mage){
+        this.heroName = name;
+        this.heroHealth = health;
+        this.heroAttack = attack;
+        this.heroPotion = potionStrength;
+        this.potionCount = numPotions;
+        this.goldPoint = goldPoint;
+        this.warrior = warrior;
+        this.mage = mage;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public int getHeroHealth() {
+        return heroHealth;
+    }
+
+    public int getHeroAttack() {
+        return heroAttack;
+    }
+
+    public int getHeroPotion() {
+        return heroPotion;
+    }
+
+    public int getPotionCount() {
+        return potionCount;
+    }
+
+    public int getGoldPoint() {
+        return goldPoint;
+    }
+
+    public boolean isWarrior() {
+        return warrior;
+    }
+
+    public boolean isMage() {
+        return mage;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public void setHeroHealth(int heroHealth) {
+        this.heroHealth = heroHealth;
+    }
+
+    public void setHeroAttack(int heroAttack) {
+        this.heroAttack = heroAttack;
+    }
+
+    public void setHeroPotion(int heroPotion) {
+        this.heroPotion = heroPotion;
+    }
+
+    public void setPotionCount(int potionCount) {
+        this.potionCount = potionCount;
+    }
+
+    public void setGoldPoint(int goldPoint) {
+        this.goldPoint = goldPoint;
+    }
+
+    public void setWarrior(boolean warrior) {
+        this.warrior = warrior;
+    }
+
+    public void setMage(boolean mage) {
+        this.mage = mage;
+    }
 }
 
